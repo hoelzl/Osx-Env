dfdff0143cc52803ccf39db7f5d908c87332d11d
Matthias Hoelzl <tc@xantira.com>
2014-12-13 19:20:18 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Osx-Env
 =======
 
-Setting up the environment for OS X Yosemite GUI applications
+In OSX Yosemite the old methods of setting environment variables for
+GUI applications no longer work.  This directory contains a property
+list file that calls a script that launches launchctl to set the
+environment.
+
+To use this, check out this repository into and copy the file
+`com.xantira.loginscript.plist` into your folder
+`~/Library/LauncAgents`.  Modify the path in this file to point to the
+`Osx-Env` directory, modify the value of `PATH` in `setenv.sh` to the
+desired value, log out and log in again.
+
---
+++ setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+launchctl setenv PATH "/usr/local/sbin:/usr/local/bin:/Applications/Emacs.app/Contents/MacOS/bin:/Applications/Aquamacs.app/Contents/MacOS:/Applications/Aquamacs.app/Contents/MacOS/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Applications/Server.app/Contents/ServerRoot/usr/bin:/Applications/Server.app/Contents/ServerRoot/usr/sbin:/usr/local/MacGPG2/bin:/usr/texbin:/usr/local/texlive/2013/bin/x86_64-darwin:/Users/tc/.cabal/bin:/usr/local/Cellar/nimrod/0.9.2/libexec/bin:/Users/tc/Prog/C++/Emscripten/emsdk-portable:/Users/tc/Prog/C++/Emscripten/emsdk-portable/clang/e1.22.0_64bit:/Users/tc/Prog/C++/Emscripten/emsdk-portable/node/0.10.18_64bit/bin:/Users/tc/Prog/C++/Emscripten/emsdk-portable/emscripten/1.22.0:/Users/tc/Prog/Constraints/minizinc-1.6/bin:/anaconda/bin:/usr/local/share/pypy"
+launchctl setenv ORG_HOME /Users/tc/Prog/Emacs/Org-Mode
+
+
